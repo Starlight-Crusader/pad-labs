8eda80bf59ebd7a902394c3e9f83244b03ccca45
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-09-17 10:48:20 +0300

    Trying to run the cluster

--- sA/sA/settings.py
+++ sA/sA/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv(
+            'DATABASE_URL',
+        )
+    )
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
